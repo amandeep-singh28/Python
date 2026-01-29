83854f8d1449536a2799ea3f3625d98d86a62cdc
Amandeep Singh <amandeep87557@gmail.com>
2026-01-29 15:44:00 +0530

    Joins Questions

--- My-SQL/Lecture 1.sql
+++ My-SQL/Lecture 1.sql
@@ -62,3 +62,103 @@ GROUP BY City;
 
 
 
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    first_name  VARCHAR(50),
+    last_name   VARCHAR(50),
+    age         INT,
+    country     VARCHAR(50)
+);
+
+INSERT INTO Customers (customer_id, first_name, last_name, age, country) VALUES
+(1, 'John',   'Doe',       31, 'USA'),
+(2, 'Robert', 'Luna',      22, 'USA'),
+(3, 'David',  'Robinson',  22, 'UK'),
+(4, 'John',   'Reinhardt', 25, 'UK'),
+(5, 'Betty',  'Doe',       28, 'UAE');
+
+
+CREATE TABLE Orders (
+    order_id    INT PRIMARY KEY,
+    item        VARCHAR(50),
+    amount      INT,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+
+INSERT INTO Orders (order_id, item, amount, customer_id) VALUES
+(1, 'Keyboard',  400,   4),
+(2, 'Mouse',     300,   4),
+(3, 'Monitor',   12000, 3),
+(4, 'Keyboard',  400,   1),
+(5, 'Mousepad',  250,   2);
+
+CREATE TABLE Shippings (
+    shipping_id INT PRIMARY KEY,
+    status      VARCHAR(20),
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+INSERT INTO Shippings (shipping_id, status, customer_id) VALUES
+(1, 'Pending',   2),
+(2, 'Pending',   4),
+(3, 'Delivered', 3),
+(4, 'Pending',   5),
+(5, 'Delivered', 1);
+
+
+
+SELECT C.first_name, O.item, O.item
+FROM Customers C
+INNER JOIN Orders O
+ON C.customer_id = O.customer_id;
+
+SELECT C.first_name
+FROM Customers C
+INNER JOIN Orders O
+on C.customer_id = O.customer_id;
+
+SELECT c.first_name, c.last_name, count(o.order_id) AS Number_Of_Orders
+FROM Customers c
+INNER JOIN Orders o
+on c.customer_id = o.customer_id
+GROUP BY c.customer_id;
+
+SELECT 
+    c.first_name,
+    c.last_name,
+    SUM(o.amount) AS total_amount
+FROM Customers c
+INNER JOIN Orders o
+    ON c.customer_id = o.customer_id
+GROUP BY 
+    c.customer_id,
+    c.first_name,
+    c.last_name
+HAVING SUM(o.amount) > 500;
+
+
+-- FIND CUSTOMERS WHO ORDERD ITEMS NOT ORDERED BY ANY USA CUSTOMER
+
+SELECT c.customer_id from Customers C WHERE Country not like "%USA%";
+
+
+-- FIND CUSTOMERS WHO ORDERED THE SAME ITEM AS JOHN REINHARDT
+SELECT c.customer_id FROM CUSTOMERS;
+
+
+-- Customer, order item, and shipping status
+SELECT c.first_name, o.item, s.status
+FROM Customers c
+JOIN Orders o     ON c.customer_id = o.customer_id
+JOIN Shippings s  ON c.customer_id = s.customer_id;
+
+
+
+
+
+
+
+
