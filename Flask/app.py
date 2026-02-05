864c0f375fe974889b1eb1006b0b2d4c876e34e8
Amandeep Singh <amandeep87557@gmail.com>
2026-02-05 15:29:36 +0530

    Jinja Templates

--- Flask/app.py
+++ Flask/app.py
@@ -4,22 +4,13 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 
-def feedback():
-    error = None
+def dashboard():
+    user = {
+        'name' : 'Alex',
+        'logged_in' : True,
+        'hobbies' : ['Reading', 'Cycling', 'Gaming']
+    }
+    return render_template('dashboard.html', user = user)
 
-    if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        message = request.form.get('message')
 
-        if not name or not email or not message:
-            error = "All fields are required"
-        elif '@' not in email:
-            error = "Invalid email address"
-        else:
-            print(name, email, message)
-            print("Hello")
-            return render_template('thankyou.html', name = name)
-
-    return render_template('feedback.html', error = error)
 
