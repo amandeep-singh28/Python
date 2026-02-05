d8264719083b9d7ef23e076f7e3cf2547f62b22d
Amandeep Singh <amandeep87557@gmail.com>
2026-02-05 14:41:15 +0530

    Template creation in Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,39 +1,8 @@
-from flask import Flask, redirect , url_for, session, Response, request
+from flask import Flask, render_template
 
-app = Flask(__name__) # This intializes our application
-app.secret_key = 'supersecretkey'
+app = Flask(__name__)
 
-@app.route('/', methods = ['POST', 'GET'])
-
-def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        if username == 'amandeep' and password == '123':
-            session['user'] = username
-            return redirect(url_for('welcome'))
-        else:
-            return Response('Invalid credentials ', 401)
-        
-    return '''
-        <h2> Login page </h2>
-        <form method = "post">
-            Username: <input type = "text" name = "username"><br>
-            Password: <input type = "password" name = "password"><br>
-            <input type = "submit" value = "Login">
-        </form>
-    '''
-
-@app.route('/welcome')
-def welcome():
-    if 'user' in session:
-        return f'''
-        <h2>Wekcone, {session["user"]}!</h2>
-        <a href = {url_for('logout')}>Logout</a>
-        '''
-    return redirect(url_for('login'))
-
-@app.route('/logout')
-def logout():
-    session.pop('user', None)
-    return redirect(url_for('login'))
+@app.route('/')
+def printHello():
+    return render_template("hello.html")
+    # return "Hello World"
