e0054370a48127f1a59a3f559d7f876ec129c79b
Amandeep Singh <amandeep87557@gmail.com>
2026-02-06 09:53:14 +0530

    Updation

--- Flask/app.py
+++ Flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/')
 
 def dashboard():
     user = {
@@ -10,7 +10,21 @@ def dashboard():
         'logged_in' : True,
         'hobbies' : ['Reading', 'Cycling', 'Gaming']
     }
-    return render_template('dashboard.html', user = user)
+
+    number = 430
+    return render_template('dashboard.html', user = user, number = number)
 
 
+@app.route('/loop')
+def loop():
+    l1 = [1, 2, 3, 4, 5]
+    return render_template('loop.html', l1 = l1)
+
+@app.route('/if')
+def if_condition():
+    number = 430
+    return render_template('if.html', number = number)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
