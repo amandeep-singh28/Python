2987db9871a546e4f6ea0073ae8fdebb9b26c8db
Amandeep Singh <amandeep87557@gmail.com>
2026-02-12 14:36:00 +0530

    Working of App and Application

--- Django/myproject/myproject/urls.py
+++ Django/myproject/myproject/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('baigan/', views.say_hello),
     path('', views.say_goodmorning),
+    path('app/', include('app.urls'))
 ]
--- Django/myproject/myproject/views.py
+++ Django/myproject/myproject/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, request
-
+from django.shortcuts import render
 def say_hello(request):
     return HttpResponse("Hello World")
 
 def say_goodmorning(request):
-    return HttpResponse("Good Morning")
+    return render(request, 'index.html')
