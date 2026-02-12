38eaf145b0778af4360e876f17ed52199f177f9a
Amandeep Singh <amandeep87557@gmail.com>
2026-02-12 15:01:20 +0530

    Linking App2 with main application

--- Django/myproject/myproject/urls.py
+++ Django/myproject/myproject/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('baigan/', views.say_hello),
     path('', views.say_goodmorning),
-    path('app/', include('app.urls'))
+    path('app/', include('app.urls')),
+    path('app2/', include('app2.urls'))
 ]
