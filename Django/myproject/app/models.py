9de67b7cca6186e2829c44b9c9e5ee7fa0b13f4e
Amandeep Singh <amandeep87557@gmail.com>
2026-02-13 11:15:42 +0530

    Linking a table with superuser

--- Django/myproject/app/models.py
+++ Django/myproject/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+
+    Branch_choices = [
+        ('CS', 'Computer Science'),
+        ('ELEC', 'Electronics'),
+        ('CIVIL', 'Civil Engineering')
+    ]
+
+    name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    age = models.IntegerField()
+    branch = models.CharField(max_length = 20, choices = Branch_choices)
+
+    def __str__(self):
+        return self.name
