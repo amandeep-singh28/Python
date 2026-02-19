3cea3382b56497113b7267885fda2e3ff4039db8
Amandeep Singh <amandeep87557@gmail.com>
2026-02-19 09:59:20 +0530

    Django Filter settings

--- DjangoAPI/myproject/myproject/settings.py
+++ DjangoAPI/myproject/myproject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "app"
+    "app",
+    "django_filters"
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS' : [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ]
+}
