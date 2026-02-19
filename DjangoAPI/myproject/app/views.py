3beaa8b6fc1b3a50ee2fe7251e555453581d72f8
Amandeep Singh <amandeep87557@gmail.com>
2026-02-19 09:58:46 +0530

    Django_Filter code

--- DjangoAPI/myproject/app/views.py
+++ DjangoAPI/myproject/app/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Student
+from .serializers import ProductSerializer, StudentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'price']
 
 class StudentViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
