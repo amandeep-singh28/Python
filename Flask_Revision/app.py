3929aef2b22ba1407ac41630251444802059579c
Amandeep Singh <amandeep87557@gmail.com>
2026-02-25 10:04:28 +0530

    Manual Form Validation

--- Flask_Revision/app.py
+++ Flask_Revision/app.py
@@ -4,12 +4,21 @@ app = Flask(__name__)
 
 @app.route("/", methods = ["POST", "GET"])
 def home():
+    error = None
+    
     if request.method == "POST":
         name = request.form.get("username")
         age = request.form.get("age")
-        return f"Good morning {name}, you are {age} years old"
+        
+        # Validation
+        if not name:
+            error = "Name is required"
+        elif not age:
+            error = "Age is required"
+        else:
+            return f"Hello {name}, you are {age} years old"
     
-    return render_template("form.html")
+    return render_template("form.html", error = error)
 
 if __name__ == "__main__":
     app.run(debug = True)
