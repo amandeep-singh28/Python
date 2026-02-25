eb10e175a52a83a8c890813874adae656b29e190
Amandeep Singh <amandeep87557@gmail.com>
2026-02-25 10:14:10 +0530

    Post-Redirect-Get Functionality

--- Flask_Revision/app.py
+++ Flask_Revision/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["POST", "GET"])
 def home():
     error = None
-    
+
     if request.method == "POST":
         name = request.form.get("username")
         age = request.form.get("age")
@@ -16,9 +16,15 @@ def home():
         elif not age:
             error = "Age is required"
         else:
-            return f"Hello {name}, you are {age} years old"
+            return redirect(url_for("details", username = name, age = age))
     
     return render_template("form.html", error = error)
+
+@app.route("/details")
+def details():
+    name = request.args.get("username")
+    age = request.args.get("age")
+    return f"Hello {name}, you are {age} years old!"
 
 if __name__ == "__main__":
     app.run(debug = True)
